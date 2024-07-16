9f52d8ac4f5376678f2ecbb3de348b7f987b8b15
mxrsel <marsel14082006@gmail.com>
2024-07-16 13:19:35 +0600

    added elements on page

--- src/Todo/Todo.tsx
+++ src/Todo/Todo.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Todo.css'
 
-const Todo = () => {
+const Todo: React.FC = () => {
   return (
-    <div>
-      
+    <div className="todo">
+      <h1 className="main-title">To Do list</h1>
+      <form>
+        <input
+          type="text"
+          />
+
+        <button className="btn btn-add" type="submit">Add</button>
+      </form>
+      <ul>
+        <li>
+          <input
+          type="checkbox"
+          />
+          <button>Delete</button>
+        </li>
+      </ul>
     </div>
   );
 };
